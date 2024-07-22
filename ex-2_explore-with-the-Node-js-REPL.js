c3dd7ccc89147686afc8e1a1c6c6e737a216d9f4
AndreaSchiariti <schiariti.andrea@gmail.com>
2024-07-22 12:49:08 +0200

    solved exercise 2

---
+++ ex-2_explore-with-the-Node-js-REPL.js
@@ -0,0 +1,8 @@
+console.log(crypto.randomUUID())
+
+/* Comandi eseguiti sul terminale per navigare il REPL:
+
+node -invio-
+crypto. -tab tab-
+
+questo mi ha permesso sul terminale di vedere il metodo randomUUID */
